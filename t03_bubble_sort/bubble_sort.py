66e17f25edebafd6847b8b0ae7476eceaa49b33e
IvanNemirovskiy <65948557+IvanNemirovskiy@users.noreply.github.com>
2021-09-07 13:56:50 +0300

    Delete t03_bubble_sort directory

--- t03_bubble_sort/bubble_sort.py
+++
@@ -1,6 +0,0 @@
-def bubble_sort(arr):
-    l = len(arr)
-    for i in range(l - 1):
-        for j in range(0, l - i - 1):
-            if arr[j] > arr[j + 1]:
-                arr[j], arr[j + 1] = arr[j + 1], arr[j]
