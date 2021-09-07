c1ec4451ace1620b38cd590ee2d558fccf60c1f8
IvanNemirovskiy <65948557+IvanNemirovskiy@users.noreply.github.com>
2021-09-07 13:55:51 +0300

    Delete t00_tuple directory

--- t00_tuple/song.py
+++
@@ -1,6 +0,0 @@
-def song(verses : tuple, chorus: tuple) -> tuple:
-    output = ()
-    if isinstance(verses, tuple) and isinstance(chorus, tuple):
-        for v in verses:
-            output += v + chorus
-        return  output + chorus
