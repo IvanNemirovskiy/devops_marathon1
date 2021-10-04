504e539040dc96ad92274dce3280d60c145668f2
IvanNemirovskiy <65948557+IvanNemirovskiy@users.noreply.github.com>
2021-10-04 19:29:18 +0300

    Update gather.py

--- network/gather.py
+++ network/gather.py
@@ -1,11 +1,7 @@
-import urllib3
-from virl2_client import ClientLibrary
+import netmiko
 
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-client = ClientLibrary("https://10.10.20.161", "developer", "C1sco12345", ssl_verify=False)
-lab = client.create_lab()
-print(lab.id)
-s1 = lab.create_node("s1", "server", 50, 100)
-s2 = lab.create_node("s2", "server", 50, 200)
-print(s1, s2)
-lab.connect_two_nodes(s1, s2)
+connection = netmiko.ConnectHandler(ip='10.10.20.175', device_type="cisco_ios", username='cisco', password="cisco")
+connection.enable()
+serial = connection.send_command('sh version')
+print(serial)
+print(connection.send_command('ping 10.10.20.175'))
