3872f0981dc1a87757869a71ad2d786297d6df66
IvanNemirovskiy <65948557+IvanNemirovskiy@users.noreply.github.com>
2021-11-02 14:43:22 +0200

    Delete network directory

--- network/gather.py
+++
@@ -1,7 +0,0 @@
-import netmiko
-
-connection = netmiko.ConnectHandler(ip='10.10.20.175', device_type="cisco_ios", username='cisco', password="cisco")
-connection.enable()
-serial = connection.send_command('sh version')
-print(serial)
-print(connection.send_command('ping 10.10.20.175'))
--- network/vlan.sh
+++
@@ -1,6 +0,0 @@
-configuration terminal
-vlan 10
-name NewVLAN
-exit
-exit
-wr
